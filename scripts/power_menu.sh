08af42724dde497edc60009182504b1139524fec
Arthur Sonzogni <sonzogniarthur@gmail.com>
2026-06-11 09:43:22 +0200

    feat: add themed dunst notifications and icon-based power menu

--- scripts/power_menu.sh
+++ scripts/power_menu.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Define options
-LOCK="🔒 Lock"
-LOGOUT="🚪 Logout"
-SUSPEND="😴 Suspend"
-REBOOT="🔄 Reboot"
-SHUTDOWN="🔌 Shutdown"
+LOCK="  Lock"
+LOGOUT="  Logout"
+SUSPEND="  Suspend"
+REBOOT="  Reboot"
+SHUTDOWN="  Shutdown"
 
 options="$LOCK\n$LOGOUT\n$SUSPEND\n$REBOOT\n$SHUTDOWN"
 
 # Show rofi menu
-chosen=$(echo -e "$options" | rofi -dmenu -i -p "Power Menu" -theme ~/.config/i3/rofi/theme.rasi -theme-str 'window {width: 15%;} listview {lines: 5;}')
+chosen=$(echo -e "$options" | rofi -dmenu -i -p "Power" -theme ~/.config/i3/rofi/config.rasi -theme-str 'window {width: 12%;} listview {lines: 5;}')
 
 case "$chosen" in
     "$LOCK")
